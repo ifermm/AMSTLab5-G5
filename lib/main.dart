e079252466da631b00b5664a5e2d2157e04b9f9e
ifermm <ifmieles@espol.edu.ec>
2022-08-11 10:22:29 -0500

    Lab5 Grupo5

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:g5amst/ui/exampleapp.dart';
+
+void main() {
+  runApp(const ExampleApp());
+}
---
+++ lib/ui/routes.dart
@@ -0,0 +1,46 @@
+import 'package:g5amst/models/product.dart';
+import 'package:g5amst/ui/views/register/register_view.dart';
+import 'package:g5amst/ui/views/cart/cart_view.dart';
+import 'package:g5amst/ui/views/home/home_view.dart';
+import 'package:g5amst/ui/views/login/login_view.dart';
+import 'package:g5amst/ui/views/product/product_view.dart';
+import 'package:g5amst/ui/views/splash/splash.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => SplashView());
+      case '/login':
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case '/register':
+        return MaterialPageRoute(builder: (_) => Registerview());
+      case '/home':
+        return MaterialPageRoute(builder: (_) => HomeView());
+      case '/product-view':
+        Product _product = args as Product;
+        return MaterialPageRoute(builder: (_) => ProductView(_product));
+      case '/cart-view':
+        return MaterialPageRoute(builder: (_) => CartView());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Error"),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text("ERROR"),
+        ),
+      );
+    });
+  }
+}
